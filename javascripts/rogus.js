6e46af684666da8f59da37c25ab2f4bcf90d296b
mariosiaci <mario.siaci@gmail.com>
2014-01-31 00:03:18 -0200

    Teste Jquery
    Testes

---
+++ javascripts/rogus.js
@@ -0,0 +1,47 @@
+function moneyTextToFloat(text) {
+	var cleanText = text.replace("R$ ", "").replace(",", ".");
+	return parseFloat(cleanText);
+}
+
+function floatToMoneyText(value) {
+	var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
+	text = "R$ " + text;
+	return text.substr(0, text.length - 2) + "," + text.substr(-2);
+}
+
+function readTotal() {
+	var total = $("#total").text();
+	return moneyTextToFloat(total);
+}
+
+function writeTotal(value) {
+	var text = floatToMoneyText(value);
+	$('#total').text(text);
+}
+
+function calculateTotalProducts() {
+	var produtos = $(".produto");
+
+	var total = 0;
+
+	$(produtos).each(function(pos,produto) {
+		var $produto = $(produto);
+		var quantity = moneyTextToFloat(
+			$produto.find('.quantity').val());
+		var price = moneyTextToFloat(
+			$produto.find(".price").text());
+		total +=quantity*price;
+	});
+	return total;
+}
+
+function onQuantityChange() {
+	writeTotal(calculateTotalProducts());
+}
+
+$(function() {
+  $(".quantity").change(function() {
+    writeTotal(calculateTotalProducts());
+  });
+});
+ // window.onload = onDocumentLoad;
